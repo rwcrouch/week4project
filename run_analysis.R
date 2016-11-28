8e42eedc9df9ab979d1c1744c74f490d4c923e6f
Randall Crouch <Randall Crouch>
2016-11-28 01:36:13 -0500

    Script

--- run_analysis.R
+++ run_analysis.R
@@ -9,27 +9,32 @@ actlab<-read.table("activity_labels.txt")
 setwd("train")
 train<-read.table("X_train.txt")
 ytrain<-read.table("y_train.txt")
+subtrain<-read.table("subject_train.txt")
+
 setwd("..")
 setwd("test")
 test<-read.table("X_test.txt")
 ytest<-read.table("y_test.txt")
+subtest<-read.table("subject_test.txt")
 
 #assigning variable names
 names(train)<-features$V2
 names(test)<-features$V2
 
-#merging data on activity type
+#merging data on activity type and subject
 train<-cbind(ytrain,train)
 test<-cbind(ytest,test)
+train<-cbind(subtrain,train)
+test<-cbind(subtest,test)
 
 #merging train and test data
 both<-rbind(train,test)
 
-
-names(both)[1]<-"activity"
+names(both)[1]<-"subject"
+names(both)[2]<-"activity"
 
 #only keeping mean, std and activity variables
-both1<-both[grepl("mean\\(|std|activity",names(both))]
+both1<-both[grepl("mean\\(|std|activity|subject",names(both))]
 
 #tidying variable names
 names(both1)<-tolower(names(both1))
@@ -44,5 +49,5 @@ both1<-mutate(both1,activity=factor(6*(activity),labels=actlab$V2))
 
 
 #creating a new data set with the mean of each variable for each activity
-byact<- group_by(both1, activity) 
+byact<- group_by(both1, subject, activity) 
 means<-summarize_each(byact, funs(mean(., na.rm=TRUE)))
